9787f701898e57ec84ba115cb803ea661dcf5fa1
Andrei Korostelev <andrei@korostelev.net>
2014-10-12 18:12:53 +0200

    More robust startup script

--- restart_ccpy.sh
+++ restart_ccpy.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-cd $( dirname "${BASH_SOURCE[0]}" )
-[ -f /var/run/ccpyd.pid ] && kill `cat /var/run/ccpyd.pid`
-./ccpyd.py $1
+function stop_ccpy()
+{
+    if [ -f /var/run/ccpyd.pid ] ; then
+        local pid=$(cat /var/run/ccpyd.pid)
+        if kill -0 ${pid} ; then
+            kill ${pid}
+            sleep 0.5
+            if kill -0 ${pid} > /dev/null 2>&1; then
+                kill -9 ${pid}
+            fi
+        fi
+    fi
+}
+
+function start_ccpy()
+{
+    cd $( dirname "${BASH_SOURCE[0]}" )
+    ./ccpyd.py $1
+}
+
+stop_ccpy
+start_ccpy $1
