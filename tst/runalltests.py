250d113bd923813b0bb2dda3bbd5ca00cc0d4762
Andrei Korostelev <andrei@korostelev.net>
2014-10-20 19:56:47 +0200

    Renamed common test runner

--- tst/runalltests.py
+++ tst/runalltests.py
@@ -1,14 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
 #
-#  HeadURL : $HeadURL: svn://korostelev.net/CcPy/Trunk/tst/alltests.py $
-#  Id      : $Id: alltests.py 87 2009-02-04 20:46:23Z akorostelev $
-#
-#  Copyright (c) 2008-2009, Andrei Korostelev <andrei at korostelev dot net>
+#  Copyright (c) 2008-2014, Andrei Korostelev <andrei at korostelev dot net>
 #
 #  Before using this product in any way please read the license agreement.
 #  If you do not agree to the terms in this agreement you are not allowed
 #  to use this product or parts of it. You can read this license in the
 #  file named LICENSE.
 #
+
 
 """
 Executes all unit tests in the current directory
